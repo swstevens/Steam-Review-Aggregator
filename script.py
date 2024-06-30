bc939836c1afe36fae778303dfb9614b3d2fe2f4
swstevens <sstevensw@gmail.com>
2024-06-30 13:02:48 -0700

    note taking and basic api call

--- script.py
+++ script.py
@@ -1,17 +1,45 @@
 import requests
 
-# Take user input
-user_input = input("Enter the endpoint: ")
+# https://store.steampowered.com/api/appdetails?appids=2519060
+# this api lets you see all information about a game
 
-# Construct the API URL
-url = f'https://api.example.com/{user_input}'
+# https://store.steampowered.com/appreviewhistogram/275850?l=english
+# this api lets you see the review histogram of a game
 
-response = requests.get(url)
+# https://steamcommunity.com/actions/SearchApps/{search_term}
+# this api lets you search for a game by name and find its appid
 
-# Check if the request was successful
-if response.status_code == 200:
-    print('Success!')
-    data = response.json()
-    # Now you can work with the data
-else:
-    print('Failed to retrieve data')
+'''
+take in a name, and replace space character with %20, then call searchapps
+then list all the response name and let the user choose one
+then run the batch review script
+
+review script:
+quick look at reviews
+process the reviews and remove superfluous reviews. bad review indicators to be decided.
+'''
+
+def main():
+    while True:
+        # Take user input
+        user_input = input("Enter the endpoint: ")
+
+        if user_input == 'exit':    # Exit the program  
+            break
+
+        # Construct the API URL
+        url = f'https://store.steampowered.com/appreviews/{user_input}?json=1'
+
+        response = requests.get(url)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            print('Success!')
+            data = response.json()
+            print(data)
+            # Now you can work with the data
+        else:
+            print('Failed to retrieve data')
+
+if __name__ == '__main__':
+    main()
